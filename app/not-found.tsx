d502948df3608f07dc48f36ec0db4d6a0f1706e4
Alex <alex.pober.95@gmail.com>
2024-12-03 20:54:36 -0800

    added proper not found page for vercel

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+ 
+export default function NotFound() {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center space-y-4">
+      <h2 className="text-2xl font-bold">Not Found</h2>
+      <p>Could not find the requested resource</p>
+      <Link 
+        href="/"
+        className="text-blue-500 hover:text-blue-700 underline"
+      >
+        Return Home
+      </Link>
+    </div>
+  )
+}
